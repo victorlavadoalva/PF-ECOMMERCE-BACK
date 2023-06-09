9ac37b4af950aff84a1b714e4b06fbda9883ac4d
victor <victor.lavado15@hotmail.com>
2023-06-08 23:09:27 -0500

    agregado params a la ruta de getProduct

--- src/routes/productsRoutes/handlers/getHandlerProducts.js
+++ src/routes/productsRoutes/handlers/getHandlerProducts.js
@@ -1,12 +1,14 @@
 const getControllerProducts = require("../controllers/getControllerProducts");
 
 const getHandlerProducts = async (req, res) => {
-    try {
-        const productsData = await getControllerProducts();
-        res.status(200).json(productsData)
-    } catch (error) {
-        res.status(500).json({ err:error.message });
-    }
+  const { name } = req.query;
+
+  try {
+    const productsData = await getControllerProducts(name);
+    res.status(200).json(productsData);
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
 };
 
 module.exports = getHandlerProducts;
