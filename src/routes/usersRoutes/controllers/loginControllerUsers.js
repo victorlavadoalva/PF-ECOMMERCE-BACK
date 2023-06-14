ba2cc9060e3367f1b09a2d8d107c29e406d11f8a
victor <victor.lavado15@hotmail.com>
2023-06-13 20:51:59 -0500

    Agregado cart en User

--- src/routes/usersRoutes/controllers/loginControllerUsers.js
+++ src/routes/usersRoutes/controllers/loginControllerUsers.js
@@ -4,6 +4,7 @@ const loginControllerUsers = async (email, password) => {
   console.log(email);
   console.log(password);
   const user = await User.findByCredentials(email, password);
+  console.log(user);
   return user;
 };
 
