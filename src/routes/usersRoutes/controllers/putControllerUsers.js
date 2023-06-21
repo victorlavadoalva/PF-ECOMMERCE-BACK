4c73da98fbff61eab448df3f7a09efd332f37aa1
victor <victor.lavado15@hotmail.com>
2023-06-21 18:36:37 -0500

    cambios para modificar user

--- src/routes/usersRoutes/controllers/putControllerUsers.js
+++ src/routes/usersRoutes/controllers/putControllerUsers.js
@@ -3,21 +3,31 @@ const Users = require("../../../db/models/User");
 
 const putControllerUsers = async (id, data) => {
   if (!id) throw new Error("Enter an id");
-  const galleta =
-    data.name || data.email || data.password || data.isAdmin || data.cart;
+  const galleta = data.name || data.email || data.password;
   if (!galleta) throw new Error("Enter data to modify");
 
   const idParsed = parseID(id);
 
-  const dataModify = await products.findByIdAndUpdate(idParsed, {
-    name: data.name,
-    email: data.email,
-    password: data.password,
-    isAdmin: data.isAdmin,
-    cart: data.cart,
-  });
+  try {
+    const user = await Users.findById(idParsed);
+    if (!user) throw new Error("User not found");
 
-  return dataModify;
+    if (data.name) user.name = data.name;
+    if (data.email) user.email = data.email;
+    if (data.password) {
+      user.password = data.password;
+      await user.save(); // Guarda el usuario para ejecutar el hook de pre-save y encriptar la contraseña
+    } else {
+      await user.save(); // Guarda el usuario sin encriptar la contraseña
+    }
+
+    return user;
+  } catch (error) {
+    if (error.code === 11000 && error.keyValue && error.keyValue.email) {
+      throw new Error("Email already exists");
+    }
+    throw error;
+  }
 };
 
 module.exports = putControllerUsers;
