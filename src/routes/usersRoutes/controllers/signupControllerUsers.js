c2fb43f1a5c11dedda2e53367f7eeccbf125e017
Kris0707 <francokris005@gmail.com>
2023-06-21 21:00:45 -0300

    Manejo de errores USER && REGISTER

--- src/routes/usersRoutes/controllers/signupControllerUsers.js
+++ src/routes/usersRoutes/controllers/signupControllerUsers.js
@@ -1,8 +1,22 @@
 const User = require("../../../db/models/User");
 
+
 const signupControllerUsers = async (name, email, password) => {
+  const foundEmail = await User.findOne({email})
+  if(foundEmail) {
+    const error = new Error("Email ya existe");
+    error.EmailExist = true;
+    throw error;
+  }
+  const foundName = await User.findOne({name})
+  if(foundName) {
+    const error = new Error("Name ya existe");
+    error.NameExist = true;
+    throw error;
+  }
   const user = await User.create({ name, email, password });
   return user;
 };
 
-module.exports = signupControllerUsers;
+module.exports = signupControllerUsers
+
