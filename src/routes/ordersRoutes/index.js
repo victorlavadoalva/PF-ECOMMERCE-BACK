394a62d3d7c5fcb68d9bc84d2dbc1db171c2963a
victor <victor.lavado15@hotmail.com>
2023-07-04 19:47:57 -0500

    agregado a la ruta para que no muestre 2 veces el mismo id

--- src/routes/ordersRoutes/index.js
+++ src/routes/ordersRoutes/index.js
@@ -68,26 +68,26 @@ api.post("/", async (req, res) => {
 
 // Obteniendo todas las ordenes;
 api.get("/", async (req, res) => {
- const {id}= req.query
- console.log(id);
- if(id) {
-  const orders = await Order.find({owner:id})
-  const filteredProperties = orders.map(order => {
-    const keys = Object.keys(order.products);
-    return keys.filter(key => key !== "count" && key !== "total");
-  });
-  
-  res.status(200).json(filteredProperties.flat())
- }else {
-  try {
-    const orders = await Order.find().populate("owner", ["email", "name"]);
-    res.status(200).json(orders);
-  } catch (e) {
-    res.status(400).json(e.message);
-  }
- }
-  
-});
+  const { id } = req.query;
+  console.log(id);
+  if (id) {
+    const orders = await Order.find({ owner: id });
+    const filteredProperties = orders.map((order) => {
+      const keys = Object.keys(order.products);
+      return keys.filter((key) => key !== "count" && key !== "total");
+    });
 
+    const uniqueItems = [...new Set(filteredProperties.flat())];
+
+    res.status(200).json(uniqueItems);
+  } else {
+    try {
+      const orders = await Order.find().populate("owner", ["email", "name"]);
+      res.status(200).json(orders);
+    } catch (e) {
+      res.status(400).json(e.message);
+    }
+  }
+});
 
 module.exports = api;
